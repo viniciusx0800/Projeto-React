38e1bd8564851a9b630df79077ce72a76ac60366
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-11-26 11:42:10 -0300

    projeto em andamento

--- gitfind/src/pages/Home/index.js
+++ gitfind/src/pages/Home/index.js
@@ -1,7 +1,47 @@
 import { Header } from '../../Components/Header';
+import github from '../../assets/github.png';
+import ItemList from '../../Components/ItemList'
+import './styles.css'
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header/>
+      <div className="conteudo">
+      <img src={github} className="github" alt="github app"/>
+      <div className='info'>
+        <div>
+          <input name="usuario" placeholder='@username'/>
+          <button>Buscar</button>
+        </div>
+        <div className="perfil">
+          <img src="https://avatars.githubusercontent.com/u/166649039?v=4" 
+          className="profile" 
+          alt="imagen de perfil"
+          />      
+        <div> 
+          <h3>Marcos Vinicius</h3>
+          <span>@Viniciusx0800</span>
+          <p>Descrição.</p>
+         
+        </div>
+      </div>
+      <hr/>
+      <div>
+        <h4 className='repositorio'>Repositórios</h4>
+        <ItemList title="Teste1" description="Teste de descrição"/>
+        <ItemList title="Teste1" description="Teste de descrição"/>
+        <ItemList title="Teste1" description="Teste de descrição"/>
+        <ItemList title="Teste1" description="Teste de descrição"/>
+      </div>
+    </div>
+  </div> 
+</div>
+
+
+
+
+   );
 }
 
 export default App;
